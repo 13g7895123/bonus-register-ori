a33bd82a9c3b2b3b2ee9d46beeb5ac82586e0f7e
Jarvis <13g7895123@gmail.com>
2023-09-21 23:14:02 +0800

    [edit] test js

--- Pages/js/phone.js
+++ Pages/js/phone.js
@@ -1,1 +1,24 @@
 // $('#bg').css('background');
+url_param()
+
+const url_param = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sid = urlParams.get('sid')
+    console.log(sid);
+}
+
+const get_server_name = (data) => {
+    
+}
+
+const api = (data) => {
+    $.ajax({
+        type: "post",
+        url: "url",
+        data: data,
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
+}
