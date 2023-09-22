cf9cb14455fd7c3a9d10841893d119391698cbcc
Jarvis <13g7895123@gmail.com>
2023-09-22 23:04:19 +0800

    [edit] test api

--- config/config.php
+++ config/config.php
@@ -2,14 +2,10 @@
 
 //連線資料庫設定
 const SQLPORT_SQL = '3306'; //資料庫port
-const HOSTNAME_SQL = 'localhost'; //資料庫位址
-const USERNAME_SQL = 'bonus_health_insurance_user'; //帳號
-const PASSWORD_SQL = '820820'; //密碼
-const DATABASE_SQL = 'db_bonus_health_insurance'; //資料庫名稱
+const HOSTNAME_SQL = ''; //資料庫位址
+const USERNAME_SQL = ''; //帳號
+const PASSWORD_SQL = ''; //密碼
+const DATABASE_SQL = ''; //資料庫名稱
 const SESSION_TIMEOUT = 86400; //session保留時間(單位秒)
-
-//API驗證帳密
-const AUTH_USER = "test";
-const AUTH_PW = "00000000";
 
 ?>
