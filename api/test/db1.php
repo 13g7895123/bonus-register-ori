8899b2053ab75dd0b1dabd9b112de8e8bc76bb6e
yu xian <13g7895123@gmail.com>
2023-09-23 09:48:46 +0800

    test db

--- api/test/db1.php
+++ api/test/db1.php
@@ -8,17 +8,17 @@
  include_once('/../../__Class/ClassLoad.php');
 
  MYPDO::$host = '139.162.15.125';
- MYPDO::$port = '8098';
- MYPDO::$db = 'test_db';
- MYPDO::$user = 'db_user';
- MYPDO::$pwd = '820820';
+//  MYPDO::$port = '8098';
+//  MYPDO::$db = 'test_db';
+//  MYPDO::$user = 'db_user';
+//  MYPDO::$pwd = '820820';
 
- MYPDO::$table = 'test1';
- MYPDO::$data = [
-    'c1' => 1,
-    'c2' => 2,
-    '3' => 3,
- ];
- MYPDO::insert();
+//  MYPDO::$table = 'test1';
+//  MYPDO::$data = [
+//     'c1' => 1,
+//     'c2' => 2,
+//     '3' => 3,
+//  ];
+//  MYPDO::insert();
 
 ?>
