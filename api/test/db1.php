5703ac8068b061414cbc18ed33bafdd05d9d1cc9
yu xian <13g7895123@gmail.com>
2023-09-23 09:36:35 +0800

    test db

---
+++ api/test/db1.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 測試用，串接第一個DB
+ *
+ * 
+ */
+
+ include_once(__DIR__ . '/../../__Class/ClassLoad.php');
+
+ MYPDO::$host = '139.162.15.125';
+ MYPDO::$port = '8089';
+ MYPDO::$db = 'test_db';
+ MYPDO::$user = 'db_user';
+ MYPDO::$pwd = '820820';
+
+ MYPDO::$table = 'test1';
+ MYPDO::$data = [
+    'c1' => 1,
+    'c2' => 2,
+    '3' => 3,
+ ];
+ MYPDO::insert();
+
+?>
