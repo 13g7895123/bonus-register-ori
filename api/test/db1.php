b801541154b015b8230c4ea29775879e3c160e0a
yu xian <13g7895123@gmail.com>
2023-09-23 09:54:21 +0800

    test db

--- api/test/db1.php
+++ api/test/db1.php
@@ -5,18 +5,13 @@
  * 
  */
 
- include_once(__DIR__.'/../../__Class/ClassLoad.php');
+include_once(__DIR__.'/../../__Class/ClassLoad.php');
 
-echo __DIR__.'/../../__Class/ClassLoad.php';
 MYPDO::$host = '139.162.15.125';
-echo  MYPDO::$host;
-die();
-
-
-//  MYPDO::$port = '8098';
-//  MYPDO::$db = 'test_db';
-//  MYPDO::$user = 'db_user';
-//  MYPDO::$pwd = '820820';
+MYPDO::$port = '8098';
+MYPDO::$db = 'test_db';
+MYPDO::$user = 'db_user';
+MYPDO::$pwd = '820820';
 
 //  MYPDO::$table = 'test1';
 //  MYPDO::$data = [
