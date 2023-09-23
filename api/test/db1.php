9d616766e878347e90c9f20f655225e62feea93b
yu xian <13g7895123@gmail.com>
2023-09-23 10:07:24 +0800

    test db

--- api/test/db1.php
+++ api/test/db1.php
@@ -10,10 +10,10 @@ include_once(__DIR__.'/../../__Class/ClassLoad.php');
 MYPDO::$host = '139.162.15.125';
 MYPDO::$port = '8098';
 MYPDO::$db = 'test_db';
-MYPDO::$user = 'root';
-MYPDO::$pwd = 'termit0035';
-// MYPDO::$user = 'db_user';
-// MYPDO::$pwd = '820820';
+// MYPDO::$user = 'root';
+// MYPDO::$pwd = 'termit0035';
+MYPDO::$user = 'db_user';
+MYPDO::$pwd = '820820';
 
  MYPDO::$table = 'test1';
  MYPDO::$data = [
@@ -21,9 +21,7 @@ MYPDO::$pwd = 'termit0035';
     'c2' => '3',
     'C3' => '3',
  ];
-//  MYPDO::$where = ['id' => '1'];
  $results = MYPDO::insert();
-//  $results = MYPDO::select();
 
  echo json_encode($results);
 
