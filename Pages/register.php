285d399dc4bf949678922cc345d318de76c4194c
Jarvis <13g7895123@gmail.com>
2023-09-24 15:18:57 +0800

    [edit] test api

--- Pages/register.php
+++ Pages/register.php
@@ -49,3 +49,176 @@
             @click = "router.push({ path: `/phonevalidation/${server}` })"
             >取消註冊</div>
     </form>
+    <style>
+        html {
+    overflow: -moz-hidden-unscrollable;
+    height: 100%;
+}
+body::-webkit-scrollbar {
+    display: none;
+}
+body {
+    -ms-overflow-style: none;
+    height: 100%;
+	width: calc(100vw + 18px);
+	overflow: auto;
+}
+/* --- scroll bar hide end --- */
+#bg{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: url('../assets/bg.jpg') no-repeat;
+    background-size: cover;
+    background-position: center;
+    animation: bg-rotate 5s linear infinite;
+}
+
+@keyframes bg-rotate{
+    100% {
+        filter: hue-rotate(360deg);
+    }
+} 
+#mask{
+    width: calc(100% - 6px);
+    height: calc(100% - 6px);
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    border-radius: 8px;
+    background-color: black;
+    z-index: 1;
+}
+
+#register-box{
+    width: 350px;
+    height: 560px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    backdrop-filter: blur(10px);
+    overflow: hidden;
+    inset: 3px;
+    z-index: 20;
+}
+
+#register-box::before{
+    content: '';
+    position: absolute;
+    top: -50%;
+    left: -50%;
+    width: 400px;
+    height: 560px;
+    background: linear-gradient(60deg, transparent, #45f3ff, #45f3ff);
+    transform-origin: bottom right;
+    animation: animate 6s linear infinite;
+    z-index: -1;
+}
+
+#register-box::after{
+    content: '';
+    position: absolute;
+    top: -50%;
+    left: -50%;
+    width: 400px;
+    height: 560px;
+    background: linear-gradient(60deg, transparent, #d9138a, #d9138a);
+    transform-origin: bottom right;
+    animation: animate 6s linear infinite;
+    animation-delay: -3s;
+    z-index: -1;
+}
+@keyframes animate{
+    0%{
+        transform: rotate(0deg);
+    }
+    100%{
+        transform: rotate(360deg);
+    }
+}
+
+h2, #form_area{
+    z-index: 20;
+}
+
+#form_area{
+    width: 80%;
+}
+
+.inp_group{
+    /* margin-top: 0.5em; */
+    position: relative;
+}
+
+.inp_group input{
+    position: relative;
+    width: 100%;
+    padding: 20px 10px 6px 10px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    box-shadow: none;
+    color: #fff;
+    font-size: 1em;
+    letter-spacing: 0.05em;
+    transition: 0.5s;
+}
+
+.column{
+    position: absolute;
+    left: 0;
+    padding: 20px 10px 10px;
+    font-size: 1em;
+    color: #fff;
+    pointer-events: none;
+    letter-spacing: 0.05em;
+    transition: 0.5s;
+}
+
+.notice{
+    position: absolute;
+    left: none;
+    right: 0px;
+    top: 28px;
+    font-size: 0.7em;
+    color: red;
+    pointer-events: none;
+    letter-spacing: 0.05em;
+    transition: 0.5s;
+}
+
+.inp_group input:valid ~.column,
+.inp_group input:focus ~.column{
+    font-size: 0.75em;
+    transform: translate(-10px, -15px);
+}
+
+.inp_group i{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background-color: #fff;
+    width: 100%;
+    height: 2px;
+}
+
+#btn_send_code{
+    padding: 2px 10px 2px 10px;
+    border-radius: 5px;
+    position: absolute;
+    right: 0px;
+    top: 20px;
+}
+
+.btn{
+    display: flex;
+    cursor: pointer;
+    background-color: #fff;
+}
+
+.code-box{
+    position: absolute;
+    right: 0;
+    top: 14px;
+}
+</style>
