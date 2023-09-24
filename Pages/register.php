fd8e9c94d58142476c8c840214b7aa548a3c9c3c
Jarvis <13g7895123@gmail.com>
2023-09-24 18:51:22 +0800

    [edit] test api

--- Pages/register.php
+++ Pages/register.php
@@ -41,7 +41,7 @@
             <hr class="mt-5">>
             <div 
                 id="btn-submit"
-                class="bg-white rounded flex justify-center items-center btn py-1 mt-3"
+                class="bg-white rounded flex justify-center items-center btn py-1 mt-1"
                 >提交註冊</div>
             <div
                 id='btn-cancel-register'
