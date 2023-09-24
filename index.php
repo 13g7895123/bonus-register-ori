7cbc9d36c77825cda6910d25b9d8315e7e202e5e
Jarvis <13g7895123@gmail.com>
2023-09-24 13:48:06 +0800

    [edit] test api

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if (BaseWork::_get('page') != "" && file_exists('Pages/' . BaseWork::_get('page'
             </div>
         </div>
     </div> -->
-    <div class='h-[calc(100%-4rem)] pt-3 relative flex flex-col justify-center'>
+    <div class='h-screen pt-3 relative flex flex-col justify-center'>
         <div id='mask' class='w-full h-full hidden bg-slate-800 opacity-30 absolute top-0 z-20'></div>
         <!-- 顯示畫面 -->
         <?php include('Pages/' . $NOW_Page . '.php'); ?>    
