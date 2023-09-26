6ca7fe72fd1bcb88df9f4997655161cb760ac566
Jarvis <13g7895123@gmail.com>
2023-09-26 23:20:58 +0800

    [edit] test api

--- Pages/js/phone.js
+++ Pages/js/phone.js
@@ -158,7 +158,7 @@ const alertMsg = data => {
         title: '系統訊息',
         text: data.msg,
         icon: icon,
-        position: 'center',
+        position: 'top',
         timer: 2000,
     })
 }
