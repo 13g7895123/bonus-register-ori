9750de89511fb0e33f11f836c4e93adb90501d86
Jarvis <13g7895123@gmail.com>
2023-09-29 20:28:59 +0800

    test

--- Pages/js/phone.js
+++ Pages/js/phone.js
@@ -164,8 +164,8 @@ $('#btn_sendCode').click(() => {
 const alertMsg = data => {
     alert(data.msg)
     let icon = (data.type == 1) ? 'success' : 'error'
-    const openAlertData = { text: data.msg }
-    openAlert(openAlertData)
+    // const openAlertData = { text: data.msg }
+    // openAlert(openAlertData)
 }
 
 const goPage = url => {
