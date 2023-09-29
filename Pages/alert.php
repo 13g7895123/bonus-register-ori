c59206185327a54d8714ebc9b4a528458277da4a
Jarvis <13g7895123@gmail.com>
2023-09-29 20:49:22 +0800

    test

--- Pages/alert.php
+++ Pages/alert.php
@@ -51,24 +51,3 @@
     <h2 id='alert_title'>Title</h2>
     <p id='alert_text'>texttexttexttexttext</p>
 </div>
-<script>
-function openAlert(data){
-    data.title = (data.title == '') ? '系統提示' : data.title
-    data.text = (data.text == '') ? '' : data.text
-    data.delay = (data.delay == '') ? 1.5 : data.delay
-
-    $('#alert_title').text(data.title)
-    $('#alert_text').text(data.text)
-
-    $('.alert_box').css('visibility', 'visible')
-    $('.alert_box').css('transform', 'translate(-50%, -50%) scale(1.1)')
-
-    autoCloseAlert(data.delay)  // 關閉視窗
-}
-function autoCloseAlert(second){
-    setTimeout(() => {
-        $('.alert_box').css('transform', 'translate(-50%, -50%) scale(0.1)')
-        $('.alert_box').css('visibility', 'hidden')
-    }, second*1000)
-}
-</script>
--- Pages/js/alert.js
+++ Pages/js/alert.js
@@ -1,9 +1,19 @@
-let stayTime = 2
+function openAlert(data){
+    data.title = (typeof(data.title) == 'undefined') ? '系統提示' : data.title
+    data.text = (typeof(data.text) == 'undefined') ? '' : data.text
+    data.delay = (typeof(data.delay) == 'undefined') ? 1.5 : data.delay
 
-const showAlert = setTimeout(() => {
-    $('.alert_box').css('display', 'flex')
-}, 500)
+    $('#alert_title').text(data.title)
+    $('#alert_text').text(data.text)
 
-const hideAlert = setTimeout(() => {
-    $('.alert_box').css('display', 'none')
-}, 500)
+    $('.alert_box').css('visibility', 'visible')
+    $('.alert_box').css('transform', 'translate(-50%, -50%) scale(1.1)')
+
+    autoCloseAlert(data.delay)  // 關閉視窗
+}
+function autoCloseAlert(second){
+    setTimeout(() => {
+        $('.alert_box').css('transform', 'translate(-50%, -50%) scale(0.1)')
+        $('.alert_box').css('visibility', 'hidden')
+    }, second*1000)
+}
