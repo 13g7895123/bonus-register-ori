75bf13d53ac49f4b30fa76a9fc67aff2c6737077
Jarvis <13g7895123@gmail.com>
2023-10-05 18:25:58 +0800

    test

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (BaseWork::_get('page') != "" && file_exists('Pages/' . BaseWork::_get('page'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>帳號申請</title>
+    <title>Game Account</title>
 
     <link href='./dist/output.css' rel='stylesheet'> 
     <link rel="stylesheet" href="./assets/plugin/select2.min.css" />
