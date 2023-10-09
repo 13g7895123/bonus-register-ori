bea94a2bc0a4168a1e10055c097f54151e92057f
Jarvis <13g7895123@gmail.com>
2023-10-09 10:42:18 +0800

    test

--- Pages/js/phone.js
+++ Pages/js/phone.js
@@ -1,6 +1,6 @@
-import { urlParam, getServerData, getTokenData, imgDomain } from './common'
-import { api, alertMsg, goPage } from './common'
-import { varifyValidationCodeUrl } from './common'
+import { urlParam, getServerData, getTokenData, imgDomain } from './common.js'
+import { api, alertMsg, goPage } from './common.js'
+import { varifyValidationCodeUrl } from './common.js'
 
 /* 渲染伺服器名字 */
 const serverName = urlParam()
